4c11e01d0d88903c3c50673af0b9e0bb6b079dcb
RachelPontow <rapontow@gmail.com>
2018-05-05 09:26:10 -0500

    add Darrin info

---
+++ config.py
@@ -0,0 +1,8 @@
+#spotify token
+token = "insert token here"
+secret = "insert token here"
+#last fm
+api_key = "insert token here"
+shared_secret = "insert token here"
+#getsongbpm API
+bpm_key = "insert token here"
